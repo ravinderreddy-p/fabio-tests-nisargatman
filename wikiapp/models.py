c28a2306bf418115f60769dc7d8e1fb3fb8e27e0
Ravi <ravinder9.reddy@gmail.com>
2021-11-06 21:15:00 +0530

    Created models and added APIs to create and read continents

---
+++ wikiapp/models.py
@@ -0,0 +1,139 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+database_path = 'postgresql://pravinderreddy@localhost:5432/wiki-db'
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    migrate = Migrate()
+    db.init_app(app)
+    migrate.init_app(app, db)
+    db.create_all()
+
+
+'''
+Continent
+'''
+
+
+class Continent(db.Model):
+    __tablename__ = "continents"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    population = Column(Integer, unique=False, nullable=False)
+    area_in_sq_meters = Column(Float, unique=False, nullable=False)
+    created_at = Column(DateTime, unique=False, nullable=False)
+    updated_at = Column(DateTime, unique=False, nullable=False)
+    country = relationship('Country', backref='continents', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    '''
+    insert()
+        inserts a new record into a model in database
+        EXAMPLE
+            continent = Continent(name=req_name, area=req_area, population=req_population)
+            continent.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+       update()
+           updates a existing record from a model in a database
+           EXAMPLE
+               continent = Continent.query.filter(Continent.id == id).one_or_none()
+               continent.area = 10000
+               continent.update()
+       '''
+
+    def update(self):
+        db.session.commit()
+
+    '''
+       delete()
+           delete a record from a model in database
+           EXAMPLE
+               continent = Continent(id=req_id)
+               continent.delete()
+       '''
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+'''
+Country
+'''
+
+
+class Country(db.Model):
+    __tablename__ = "countries"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    population = Column(Integer, unique=False, nullable=False)
+    area_in_sq_meters = Column(Float, unique=False, nullable=False)
+    hospitals_count = Column(Integer, unique=False, nullable=True)
+    national_parks_count = Column(Integer, unique=False, nullable=True)
+    created_at = Column(DateTime, unique=False, nullable=False)
+    updated_at = Column(DateTime, unique=False, nullable=False)
+    continent_id = Column(Integer, ForeignKey('continents.id'))
+    city = relationship('City', backref='countries', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+'''
+City
+'''
+
+
+class City(db.Model):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    population = Column(Integer, unique=False, nullable=True)
+    area_in_sq_meters = Column(Float, unique=False, nullable=True)
+    roads_count = Column(Integer, unique=False, nullable=True)
+    trees_count = Column(Integer, unique=False, nullable=True)
+    created_at = Column(DateTime, unique=False, nullable=False)
+    updated_at = Column(DateTime, unique=False, nullable=False)
+    country_id = Column(Integer, ForeignKey('country.id'))
+
+    def __init__(self, name):
+        self.name = name
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
--- wikiapp/views.py
+++ wikiapp/views.py
@@ -1,6 +1,32 @@
+from flask import jsonify, request
+
 from wikiapp import app
+from wikiapp.models import Continent
 
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/api/wiki/continents', methods=['GET'])
+def get_continents():
+    continents_list = []
+    continents = Continent.query.all()
+    for continent in continents:
+        continent_json_data = {
+            "id": continent.id,
+            "name": continent.name,
+            "population": continent.population,
+            "area": continent.area_in_sq_meters
+        }
+        continents_list.append(continent_json_data)
+    return jsonify({
+        "status_code": 200,
+        "continents": continents_list,
+    })
+
+
+@app.route('/api/wiki/continents', methods=['POST'])
+def create_a_continent():
+    body = request.get_json()
+    name = body.get("name")
+    population = body.get("population")
+    area = body.get("area")
+    continent = Continent(name=name, population=population, area_in_sq_meters=area)
+    continent.insert()
