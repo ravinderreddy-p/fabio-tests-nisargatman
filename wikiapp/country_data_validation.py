b7bc3ccf2bc6ee71f26649b4b649c7a283c015aa
Ravi <ravinder9.reddy@gmail.com>
2021-11-09 19:44:31 +0530

    Final commit of part-1 v1.0

--- wikiapp/country_data_validation.py
+++ wikiapp/country_data_validation.py
@@ -22,11 +22,19 @@ def country_population_and_area_are_valid(continent_id, new_country_population, 
     continent_population = continent.population
     continent_area = continent.area_in_sq_meters
 
-    existing_countries_population = Country.query.with_entities(func.sum(Country.population).label("sum")) \
-        .filter(continent_id == continent_id).first()
+    countries_count = Country.query.filter_by(continent_id=continent_id).count()
+    if countries_count == 0:
+        if continent_population >= new_country_population and continent_area >= new_country_area:
+            return True
+        else:
+            app.logger.warning(f'Country area or population exceeds total '
+                               f'Continent-ID: {continent_id} area or population')
+            abort(400)
 
+    existing_countries_population = Country.query.with_entities(func.sum(Country.population).label("sum")) \
+        .filter_by(continent_id=continent_id).first()
     existing_countries_area = Country.query.with_entities(func.sum(Country.area_in_sq_meters).label("sum")) \
-        .filter(continent_id == continent_id).first()
+        .filter_by(continent_id=continent_id).first()
 
     if continent_count_is_higher_than_countries_count(continent_population, existing_countries_population.sum,
                                                       new_country_population):
@@ -45,7 +53,7 @@ def country_population_is_valid_for_update(continent_id, country_id, population)
     continent_population = Continent.query.filter_by(id=continent_id).first().population
     current_country_population = Country.query.filter_by(id=country_id).first().population
     countries_population = Country.query.with_entities(func.sum(Country.population).label("sum")) \
-        .filter(continent_id == continent_id).first()
+        .filter_by(continent_id=continent_id).first()
     updated_countries_population = countries_population.sum - current_country_population
     updated_countries_population = updated_countries_population + population
     return updated_countries_population < continent_population
@@ -55,7 +63,7 @@ def country_area_is_valid_for_update(continent_id, country_id, area):
     continent_area = Continent.query.filter_by(id=continent_id).first().area_in_sq_meters
     current_country_area = Country.query.filter_by(id=country_id).first().area_in_sq_meters
     countries_area = Country.query.with_entities(func.sum(Country.population).label("sum")) \
-        .filter(continent_id == continent_id).first()
+        .filter_by(continent_id=continent_id).first()
     updated_countries_area = countries_area.sum - current_country_area
     updated_countries_area = updated_countries_area + area
     return updated_countries_area < continent_area
